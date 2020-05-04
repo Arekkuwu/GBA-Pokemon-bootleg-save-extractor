534243a7b871449fc3f67f30905b7ee651a0a5a2
Arekkuwu <64796165+Arekkuwu@users.noreply.github.com>
2020-05-04 19:25:12 +0200

    Add files via upload

---
+++ Pokemon/interface.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Pokemon
+{
+    partial class Software
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Software));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.Button_Load_Rom = new System.Windows.Forms.Button();
+            this.label_path = new System.Windows.Forms.Label();
+            this.checkedListBox_Games = new System.Windows.Forms.CheckedListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label_selected_game = new System.Windows.Forms.Label();
+            this.label_adress = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Button_Load_Rom
+            // 
+            this.Button_Load_Rom.AllowDrop = true;
+            this.Button_Load_Rom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Button_Load_Rom.Location = new System.Drawing.Point(118, 12);
+            this.Button_Load_Rom.Name = "Button_Load_Rom";
+            this.Button_Load_Rom.Size = new System.Drawing.Size(92, 46);
+            this.Button_Load_Rom.TabIndex = 1;
+            this.Button_Load_Rom.Text = "Load Rom";
+            this.Button_Load_Rom.UseVisualStyleBackColor = true;
+            this.Button_Load_Rom.Click += new System.EventHandler(this.Button_Load_Rom_Click);
+            // 
+            // label_path
+            // 
+            this.label_path.AutoSize = true;
+            this.label_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label_path.Location = new System.Drawing.Point(9, 90);
+            this.label_path.Name = "label_path";
+            this.label_path.Size = new System.Drawing.Size(0, 17);
+            this.label_path.TabIndex = 3;
+            // 
+            // checkedListBox_Games
+            // 
+            this.checkedListBox_Games.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkedListBox_Games.FormattingEnabled = true;
+            this.checkedListBox_Games.Items.AddRange(new object[] {
+            "Emerald",
+            "FireRed"});
+            this.checkedListBox_Games.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox_Games.Name = "checkedListBox_Games";
+            this.checkedListBox_Games.Size = new System.Drawing.Size(92, 46);
+            this.checkedListBox_Games.TabIndex = 4;
+            this.checkedListBox_Games.ThreeDCheckBoxes = true;
+            this.checkedListBox_Games.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CheckedListBox_Games_ItemCheck);
+            // 
+            // label_selected_game
+            // 
+            this.label_selected_game.AutoSize = true;
+            this.label_selected_game.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label_selected_game.Location = new System.Drawing.Point(9, 70);
+            this.label_selected_game.Name = "label_selected_game";
+            this.label_selected_game.Size = new System.Drawing.Size(0, 17);
+            this.label_selected_game.TabIndex = 5;
+            // 
+            // label_adress
+            // 
+            this.label_adress.AutoSize = true;
+            this.label_adress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label_adress.Location = new System.Drawing.Point(216, 26);
+            this.label_adress.Name = "label_adress";
+            this.label_adress.Size = new System.Drawing.Size(0, 17);
+            this.label_adress.TabIndex = 6;
+            // 
+            // Software
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 121);
+            this.Controls.Add(this.label_adress);
+            this.Controls.Add(this.label_selected_game);
+            this.Controls.Add(this.checkedListBox_Games);
+            this.Controls.Add(this.label_path);
+            this.Controls.Add(this.Button_Load_Rom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Software";
+            this.Text = "GBA Pokemon bootleg save extractor";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button Button_Load_Rom;
+        private System.Windows.Forms.Label label_path;
+        private System.Windows.Forms.CheckedListBox checkedListBox_Games;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label_selected_game;
+        private System.Windows.Forms.Label label_adress;
+    }
+}
+
---
+++ Pokemon/interface.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokemon
+{
+    public partial class Software : Form
+    {
+        public Software()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckedListBox_Games_ItemCheck(object sender, ItemCheckEventArgs e) // Make sure only one game can be selected at a time
+        {
+            for (int i = 0; i < checkedListBox_Games.Items.Count; ++i)
+                if (i != e.Index) { checkedListBox_Games.SetItemChecked(i, false); }
+            label_adress.Text = "";
+        }
+
+        private string Determine_selected_game() // Read the name of selected game
+        {
+            string s = "Default";
+
+            for (int i = 0; i <= (checkedListBox_Games.Items.Count - 1); i++)
+                if (checkedListBox_Games.GetItemChecked(i)) { s = checkedListBox_Games.Items[i].ToString(); }
+
+            return s;
+        }
+
+        private bool Game_selected_null() // Tell if a game is selected
+        {
+            bool selected = false;
+
+            if(Determine_selected_game() == "Default")
+            {
+                selected = true;
+                label_path.Text = "ERROR 02: NO GAME SELECTED";
+            }
+
+            return selected;
+        }
+
+        private byte[] Create_stuffing(int size) // Create stuffing byte
+        {
+            byte[] stuffing = new byte[size];
+
+            for (int i = 0; i < size; i++) { stuffing[i] = 255; }
+            return stuffing;
+        }
+
+        OpenFileDialog FD_select_rom = new OpenFileDialog(); 
+
+        private void Button_Load_Rom_Click(object sender, EventArgs e) // When Load Rom button is clicked
+        {
+            label_path.Text   = "";
+            label_adress.Text = "";
+            if (Game_selected_null()) { return; }                           // Verify if a game is selected in CheckedBoxList_Games
+            FD_select_rom.Filter = "gba rom (*.gba)|*.gba";                 // Restrict file choice to .gba
+
+            // Open a Dialog Window to obtain the ROM
+            if (FD_select_rom.ShowDialog() != DialogResult.OK) { return; } // If the file is a gba file
+
+            string Save_Path = FD_select_rom.FileName;          // Obtain the ROM's path
+            Save_Path = Save_Path.Remove(Save_Path.Length - 3); // Remove the extension from tha path (gba)
+            
+            BinaryReader Reader = new BinaryReader(File.OpenRead(FD_select_rom.FileName)); // Start a read of the file
+            int rom_size = unchecked((int)Reader.BaseStream.Length);                       // Determine the ROM size 
+
+            // Determine if it is a 16Mo or 32Mo ROM
+            if (rom_size == 33554432) { rom_size = 0x01FFFFE0 + 0x18; }     // 32Mo
+            else
+            {
+                if (rom_size == 16777216) { rom_size = 0x00FFFFE0 + 0x18; } // 16Mo
+                else
+                {
+                    label_path.Text = "ERROR 01: BAD DUMP !";               // Else verify your dump
+                    return;
+                }
+            }
+
+            int rom_save_adress_end = Get_rom_save_adress_end(Reader, rom_size); // Determines the ROM's address of the last save's byte
+            string game = Determine_selected_game();
+
+            switch (game) // Depending on the game
+            {
+                case "Emerald":
+                    byte[] save_start_FF = Create_stuffing(57344);
+
+                    byte[] save_end_00 = Create_stuffing(16384);
+
+                    Reader.BaseStream.Position = (rom_save_adress_end - 57343);
+                    byte[] rom_emerald_save = Reader.ReadBytes(57344);
+                    Reader.Close();
+
+                    byte[] emerald_save = new byte[save_start_FF.Length + rom_emerald_save.Length + save_end_00.Length];
+                    System.Buffer.BlockCopy(save_start_FF, 0, emerald_save, 0, save_start_FF.Length);
+                    System.Buffer.BlockCopy(rom_emerald_save, 0, emerald_save, save_start_FF.Length, rom_emerald_save.Length);
+                    System.Buffer.BlockCopy(save_end_00, 0, emerald_save, save_start_FF.Length + rom_emerald_save.Length, save_end_00.Length);
+
+                    SaveData(Save_Path, emerald_save);
+                    break;
+
+                case "FireRed":
+                    Reader.BaseStream.Position = (rom_save_adress_end - 114687); // 24379392
+
+                    byte[] rom_frrd_save = Reader.ReadBytes(114688);
+
+                    Reader.Close();
+
+                    byte[] save_end_FF = Create_stuffing(16384);
+
+                    byte[] frrd_save = new byte[rom_frrd_save.Length + save_end_FF.Length];
+                    System.Buffer.BlockCopy(rom_frrd_save, 0, frrd_save, 0, rom_frrd_save.Length);
+                    System.Buffer.BlockCopy(save_end_FF, 0, frrd_save, rom_frrd_save.Length, save_end_FF.Length);
+
+                    SaveData(Save_Path, frrd_save);
+                    break;
+
+                default:
+                    label_path.Text = "ERROR 03: GAME SELECTED NOT KNOWN";
+                    break;
+            } // END OF switch (game)
+        }         // END OF Button_Load_Rom_Click
+
+        private int Get_rom_save_adress_end(BinaryReader m_br, int m_rom_size)
+        {
+            int end_adress = 0;
+            bool not_found = true;
+
+            for (int address_tested = m_rom_size; address_tested >= 0x0000020; address_tested -= 0x20)
+            {
+                m_br.BaseStream.Position = address_tested;
+                byte[] codon_byte = m_br.ReadBytes(4);
+                string codon = codon_byte[0].ToString() + codon_byte[1].ToString() + codon_byte[2].ToString() + codon_byte[3].ToString();
+
+                if (codon == "373218")
+                {
+                    end_adress = address_tested + 0x07;
+                    address_tested = 0x0000000;
+                    label_adress.Text = "Save found ! end at @0" + end_adress.ToString("x").ToUpper(); ;   
+                    not_found = false;
+                }
+            }
+
+            if(not_found) { label_path.Text = "NO SAVE FOUND !"; }
+
+            return end_adress;
+        } // END OF Get_rom_save_adress_end
+
+        protected bool SaveData(string m_Save_Path, byte[] Data)
+        {
+            BinaryWriter Writer; // = null;
+            m_Save_Path += "sav"; // Add the extension for the save file
+
+            try
+            {
+                // Create a new stream to write to the file
+                Writer = new BinaryWriter(File.OpenWrite(m_Save_Path));
+
+                // Writer raw data                
+                Writer.Write(Data);
+                label_selected_game.Text = "Save file located at :";
+                label_path.Text = m_Save_Path;
+                Writer.Flush();
+                Writer.Close();
+            }
+            catch { return false; }
+            
+            return true;
+        } // END OF SAVEDATA
+
+    }
+}
